7f9e0ad5308e77d8c5ba8672b918d80bdebbb4b1
Jimenez-ja <jimenezz.jose7@gmail.com>
2021-08-10 13:46:12 -0500

    Cambio de nombre y de boton

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import './App.css';
 import react,{useState} from 'react';
 function App() {
  
-   const [nombre, setNombre] = useState('')
+   const [nombre, setNombre] = useState('----')
 
-  const llamaFabian = () =>{
-    setNombre("Soy un Clone")
+  const llamaJose = () =>{
+    setNombre("Jose")
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <h2>{nombre}</h2>
-        <button onClick={llamaFabian}>Soy</button>
+        <button onClick={llamaJose}>Jose</button>
       </header>
     </div>
   );
