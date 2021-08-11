b342c8ded32afa6462d11bf381292f3e21339072
davc111 <vcminato@outlook.es>
2021-08-11 12:20:57 -0500

    jajajjaa

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import react,{useState} from 'react';
 function App() {
- 
+
    const [nombre, setNombre] = useState('')
 
-  const llamaFabian = () =>{
-    setNombre("Soy un Clone")
+  const llamaDiego = () =>{
+    setNombre("Soy un aaa")
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <h2>{nombre}</h2>
-        <button onClick={llamaFabian}>Soy</button>
+        <button onClick={llamaDiego}>Soy</button>
       </header>
     </div>
   );
